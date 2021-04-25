08b1e06e27799b4437f13cf59b6ccc0418891b20
andrew0144 <80224229+andrew0144@users.noreply.github.com>
2021-04-24 20:08:56 -0400

    made the random drop strategy work and started implementing a timer

--- sim2.c
+++ sim2.c
@@ -10,44 +10,52 @@
 pthread_mutex_t cutlery[number_philo];
 pthread_t phils[number_philo];
 bool locked[number_philo];
-int drop_prob = 0.2;
 bool isEating[number_philo];
 bool hasEaten[number_philo];
+int eatNum[number_philo];
 double runtime;
-
-
-double rand_value()
-{
-    return (double)rand() / RAND_MAX;
-}
+int value;
+clock_t start;
 
 void* philosopher(void* number) {
     int my_num = *((int*)number);
-    srand(time(NULL));
-
+    int count = 0;
+    restart:;
+    for (int i = 0; i < number_philo; i++) {
+        count += eatNum[i];
+    }
     while (!hasEaten[my_num]) {
+
         printf("\n Philosopher %d is thinking.\n", my_num);
 
         pthread_mutex_lock(&cutlery[my_num]);
         printf("\nPhilosopher %d has left cutlery. \n", my_num);
-        sleep(10);
-        //if(rand_value() <= drop_prob) {
+        sleep(5);
+        if (value == my_num && !(count >= 4)) {
             pthread_mutex_unlock(&cutlery[my_num]);
             printf("\nPhilosopher %d has dropped left cutlery. \n", my_num);
-       // }
+            goto restart;
+        }
         pthread_mutex_lock(&cutlery[(my_num + 1) % number_philo]);
         printf("\nPhilosopher %d has right cutlery. \n", my_num);
 
         printf("\n Philosopher %d eating.\n", my_num);
         isEating[my_num] = true;
-        sleep(10);
+        sleep(5);
         printf("\n Philosopher %d done.\n", my_num);
         isEating[my_num] = false;
         hasEaten[my_num] = true;
+        eatNum[my_num] = 1;
 
         pthread_mutex_unlock(&cutlery[(my_num + 1) % number_philo]);
         pthread_mutex_unlock(&cutlery[my_num]);
         printf("\nPhilosopher %d no longer has cutlery.\n", my_num);
+
+        if (count >= 4) {
+            clock_t end = clock();
+            double runtime = ((double)(end - start)) / CLOCKS_PER_SEC;
+            printf("\nIt took %f seconds for all the philosophers to eat.\n", runtime);
+        }
     }
 
     return NULL;
@@ -72,20 +80,19 @@ void* detect_deadlock() {
 
 int main() {
 
+    srand(time(NULL));
+    value = rand() % 5;
+
     int i;
     int phil_num[number_philo];
-    clock_t start, end;
-    double runtime;
 
     pthread_t phils[number_philo];
     pthread_t check;
 
     for (int i = 0; i < number_philo; i++) {
         isEating[i] = false;
-    }
-
-    for (int i = 0; i < number_philo; i++) {
         hasEaten[i] = false;
+        eatNum[i] = 0;
     }
 
     void* return_val;
@@ -104,9 +111,7 @@ int main() {
     for (i = 0; i < number_philo; i++) {
         pthread_join(phils[i], &return_val);
     }
-    end = clock();
-    runtime = ((double)(end - start)) / CLOCKS_PER_SEC;
-    printf("\nIt took %f seconds for all the philosophers to eat.\n", runtime);
+    
     return 0;
 
 }
