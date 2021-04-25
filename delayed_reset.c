34d1d0e93a780c0b1eb5c44da4ddf4d7db803654
Usman Moazzam <usmanmoazzam@hotmail.com>
2021-04-24 17:49:59 -0700

    updated delayed-reset code to correctly drop chopstick upon deadlock

--- delayed_reset.c
+++ delayed_reset.c
@@ -1,14 +1,16 @@
 #include <pthread.h>
 #include <stdio.h>
-#include <stdbool.h> 
+#include <stdbool.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <time.h> 
 
 #define number_philo 5
 
 pthread_mutex_t cutlery[number_philo];
 pthread_t phils[number_philo];
-bool locked[number_philo];
-
+int locked;
+pthread_t check;
 
 void* philosopher (void* number) {
   int my_num = *((int*)number);
@@ -17,18 +19,22 @@ void* philosopher (void* number) {
     printf("\n Philosopher %d is thinking.\n",my_num);
 
     pthread_mutex_lock (&cutlery[my_num]);
+    locked++;
+    printf("locked = %d ", locked);
     printf("\nPhilosopher %d has left cutlery. \n",my_num);
-    sleep(10);
+    sleep(5);
     pthread_mutex_lock (&cutlery[(my_num + 1) %  number_philo]);
     printf("\nPhilosopher %d has right cutlery. \n",my_num);
 
     printf("\n Philosopher %d eating.\n", my_num);
-    sleep(10);
+    sleep(5);
+
     printf("\n Philosopher %d done.\n", my_num);
 
     pthread_mutex_unlock (&cutlery[(my_num + 1) % number_philo]);
 
     pthread_mutex_unlock (&cutlery[my_num]);
+    locked--;
     printf("\nPhilosopher %d no longer has cutlery.\n", my_num);
   }
 
@@ -36,32 +42,23 @@ void* philosopher (void* number) {
 }
 
 void* detect_deadlock() {
-  int locks = 0;
-  for (int i = 0; i < number_philo; i++) {
-    if (locked[number_philo] == 1) {
-      locks++;
+  srand(time(NULL));
+  while (1) {
+    if (locked == 5) {
+      int c = rand() % 5;
+      printf("Deadlock detected, dropping chopstick %d", c);
+      pthread_mutex_unlock (&cutlery[c]);
+      printf("Chopstick %d was dropped.", c);
     }
+    return NULL;
   }
-
-  sleep(5);
-
-  if (locks == number_philo) {
-    pthread_cancel(phils[number_philo]);
-  }
-
-  return 0;
 }
 
 int main () {
-
   int i;
   int phil_num[number_philo];
 
-  pthread_t phils[number_philo];
-  pthread_t check;
-
   void* return_val;
-
 
   for (i = 0; i < number_philo; i++) {
     pthread_mutex_init (&cutlery[i], NULL);
@@ -75,6 +72,7 @@ int main () {
 
   for (i = 0; i < number_philo; i++) {
     pthread_join (phils[i], &return_val);
+    pthread_join (check, &return_val);
   }
 
   return 0;
