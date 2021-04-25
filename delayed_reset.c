323a7588453f543a37b36ad03215719b607d5dc5
Usman Moazzam <usmanmoazzam@hotmail.com>
2021-04-24 18:59:11 -0700

    Update delayed_reset.c

--- delayed_reset.c
+++ delayed_reset.c
@@ -9,34 +9,29 @@
 pthread_mutex_t cutlery[number_philo];
 pthread_t phils[number_philo];
 int locked;
-int count;
 pthread_t check;
 
 void* philosopher (void* number) {
   int my_num = *((int*)number);
+  printf("\nPhilosopher %d is thinking.\n",my_num);
+  sleep(5);
+  pthread_mutex_lock (&cutlery[my_num]);
+  locked++;
+  printf("\nPhilosopher %d has left cutlery. \n",my_num);
+  sleep(1);
+  pthread_mutex_lock (&cutlery[(my_num + 1) %  number_philo]);
+  printf("\nPhilosopher %d has right cutlery. \n",my_num);
 
-  while (1) {
-    printf("\n Philosopher %d is thinking.\n",my_num);
-    pthread_mutex_lock (&cutlery[my_num]);
-    locked++;
-    void* detect_deadlock();
-    printf("\nPhilosopher %d has left cutlery. \n",my_num);
-    sleep(1);
-    pthread_mutex_lock (&cutlery[(my_num + 1) %  number_philo]);
-    printf("\nPhilosopher %d has right cutlery. \n",my_num);
+  printf("\nPhilosopher %d eating.\n", my_num);
+  sleep(5);
 
-    printf("\n Philosopher %d eating.\n", my_num);
-    sleep(5);
+  printf("\nPhilosopher %d done.\n", my_num);
 
-    printf("\n Philosopher %d done.\n", my_num);
-    count++;
+  pthread_mutex_unlock (&cutlery[(my_num + 1) % number_philo]);
 
-    pthread_mutex_unlock (&cutlery[(my_num + 1) % number_philo]);
-
-    pthread_mutex_unlock (&cutlery[my_num]);
-    locked--;
-    printf("\nPhilosopher %d no longer has cutlery.\n", my_num);
-  }
+  pthread_mutex_unlock (&cutlery[my_num]);
+  locked--;
+  printf("\nPhilosopher %d no longer has cutlery.\n", my_num);
 
   return NULL;
 }
@@ -46,7 +41,7 @@ void* detect_deadlock() {
   while (1) {
     if (locked == 5) {
       int c = rand() % 5;
-      printf("Deadlock detected, dropping Chopstick %d.", c);
+      printf("\nDeadlock detected, dropping Chopstick %d.\n", c);
       pthread_mutex_unlock (&cutlery[c]);
     }
     return NULL;
@@ -66,8 +61,9 @@ int main () {
   for (i = 0; i < number_philo; i++) {
     phil_num[i] = i;
     pthread_create (&phils[i], NULL, philosopher, &phil_num[i]);
-    pthread_create (&check[i], NULL, detect_deadlock, NULL);
   }
+  sleep(10);
+  pthread_create (&check, NULL, detect_deadlock, NULL);
 
   for (i = 0; i < number_philo; i++) {
     pthread_join (phils[i], &return_val);
@@ -75,5 +71,4 @@ int main () {
   }
 
   return 0;
-
 }
